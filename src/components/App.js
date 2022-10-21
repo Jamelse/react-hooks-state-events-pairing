40f7e7209568552ee3cfb4afe0ccfc380537e4ec
Jacob <jacobamelse@hotmail.com>
2022-10-21 16:30:17 -0500

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import video from "../data/video.js";
+import React, {useState} from 'react'
+import video  from "../data/video.js";
+import Video from './Video'
+import VideoInfo from './VideoInfo'
+import Comments from './Comments.js';
 
 function App() {
-  console.log("Here's your data:", video);
+ const [likeData, setLikeData] = useState({
+    upvotes: 9210,
+    downvotes: 185,
+  })
+const [showComments, setComments] = useState(false)
+
+function onCommentClick(){
+  setComments((showComments) => !showComments)
+}
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <Video video={video}/>
+      <VideoInfo video={video} likeData={likeData} setLikeData={setLikeData}/>
+      <Comments comments={video.comments} showComments={showComments} onCommentClick={onCommentClick}/>
     </div>
   );
 }
